9ce4391d72547267ec774ecc8b97482002cd99e8
Joshua Partogi <jpartogi@live.com>
2026-06-19 11:12:58 +1000

    Improve resources

--- test/mailers/resource_mailer_test.rb
+++ test/mailers/resource_mailer_test.rb
@@ -8,5 +8,10 @@ class ResourceMailerTest < ActionMailer::TestCase
     assert_equal [download_request.visitor_email], mail.to
     assert_match download_request.resource.title, mail.subject
     assert_match "/resource-downloads/#{download_request.token}", mail.body.encoded
+    
+    # Assert presence of new content
+    assert_match "Kalau ada pertanyaan lebih lanjut jangan sungkan untuk menghubungi saya", mail.body.encoded
+    assert_match "856 4342 8348", mail.body.encoded
+    assert_match "scrum.co.id", mail.body.encoded
   end
 end
